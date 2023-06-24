5a54ca987a6020f4b33cd7ce956874cbcab99cf9
Aleksey Kuznetsov <kuznetcov.alekcey@icloud.com>
2023-06-24 13:05:10 +0300

    Реализован Этап 1 итоговой работы.

---
+++ diploma-frontend/frontend/services.py
@@ -0,0 +1,34 @@
+class GetAdminSettings:
+    """
+    Получение административных настроек.
+    """
+    pass
+
+
+class GetListViewedProducts:
+    """
+    Сервис получение списка просмотренных товаров.
+    """
+    pass
+
+
+class AddingProductToTheCart:
+    """
+    Сервис добавление товара в карзину.
+    """
+    pass
+
+
+class AddingReviewToTheProduct:
+    """
+    Сервис добавление отзыва продукту.
+    """
+    pass
+
+
+class Payment:
+    """
+    Сервис оплаты.
+    """
+    pass
+
